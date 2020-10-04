f95ef3c2b5f5600edc8a6dbfd335b9d719ea08d4
Eddie Krill <scherenhaenden@gmail.com>
2020-10-04 22:26:49 +0200

    Draft the whole page

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,30 @@ import './App.css';
 function App() {
 
   const htmlInlineStylesConvertor = new HtmlInlineStylesConvertor();
-  htmlInlineStylesConvertor.GetReactStyleByInlineInput("");
-
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+  <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" 
+  aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
+  <ul className="navbar-nav px-3">
+    <li className="nav-item text-nowrap">
+      <a className="nav-link" href="#">Sign out</a>
+    </li>
+  </ul>
+</nav>
+
+
+
+
+
+      
       <div className="container-fluid">
   <div className="row">
     <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
