f47023b201affce01109d78ee62373cce230b1ee
Eddie Krill <scherenhaenden@gmail.com>
2020-10-09 15:14:55 +0200

    Lint

--- src/services/register-services.tsx
+++ src/services/register-services.tsx
@@ -9,22 +9,11 @@ export class RegisterService {
 
     private genericServices = new GenericServices();
 
-
     public async getInitialData(): Promise<void> {
-
 
         let data = await this.genericServices.requestGetWithoutBody(`http://localhost:5000/apipublic/Emails/init`);
 
-        console.log('data', data);
-
-
-        /*let datata= fetch(`http://localhost:5000/apipublic/Emails/init`)
-                        .then(res => res.json());
-
-                        console.log('datata', datata);*/
-
-
-                        
+        console.log('data', data);                        
         
     }
 
