85d095bbcf6fd88679c9a19cde190e1d4e1a9afa
Eddie Krill <scherenhaenden@gmail.com>
2020-10-09 15:13:25 +0200

    Create login service

---
+++ src/services/login-service.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { GenericServices } from "./generic-services";
+
+
+
+export class LoginService {
+
+
+    private genericServices = new GenericServices();
+
+
+    public async getInitialData(): Promise<void> {
+
+        let data = await this.genericServices.requestGetWithoutBody(`http://localhost:5000/apipublic/Emails/init`);
+
+        console.log('data', data);
+    }
+
+}
