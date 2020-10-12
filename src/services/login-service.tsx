bf71086d4219173be8e6f4913e9e48f1a825176c
Eddie Krill <scherenhaenden@gmail.com>
2020-10-12 10:47:43 +0200

    Draft requests

--- src/services/login-service.tsx
+++ src/services/login-service.tsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { GenericServices } from "./generic-services";
-
+import RequestGetWithoutBody, { GenericServices } from "./generic-services";
 
 
 export class LoginService {
 
-
-    private genericServices = new GenericServices();
-
+    private genericServices = new GenericServices();    
+    
 
     public async getInitialData(): Promise<void> {
+
+
+        const h= RequestGetWithoutBody(`http://localhost:5000/apipublic/Emails/init`);
+
+        console.log('hd', h);
 
         let data = await this.genericServices.requestGetWithoutBody(`http://localhost:5000/apipublic/Emails/init`);
 
