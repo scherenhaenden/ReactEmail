a7488f317be83128eb8711786f846fd6cdde17f4
Eddie Krill <scherenhaenden@gmail.com>
2020-10-13 11:48:44 +0200

    Create register user model

---
+++ EmailAppBakcEnd/EmailAppBakcEnd/ApiModels/RegisterUser.cs
@@ -0,0 +1,10 @@
+namespace EmailAppBakcEnd.ApiModels
+{
+    public class RegisterUser
+    {
+        public string User { get; set; }
+        public string FirstName{ get; set; }
+        public string LastName{ get; set; }
+        public string Password{ get; set; }
+    }
+}
