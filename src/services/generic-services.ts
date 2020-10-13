b1e042ca4a9c182044bc9da99b1b8640c1d0b9e5
Eddie Krill <scherenhaenden@gmail.com>
2020-10-13 11:45:35 +0200

    Delete function based implementations

--- src/services/generic-services.ts
+++ src/services/generic-services.ts
@@ -1,40 +1,50 @@
-/*export const RequestGetWithoutBody =  < T extends {} >(url: string):  Promise<T> => {
-
-    const requestOptions = {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-        /*body: JSON.stringify({ title: 'React POST Request Example' })* /
-    };
-    return responseFetcher(url, requestOptions);
-}
-
-export default RequestGetWithoutBody 
-
-const responseFetcher = < T extends {} >(url: string, requestOptions: any): Promise<T> =>{
-
-    return fetch(url, requestOptions)
-           .then(response => response.json())  as Promise<T>;
-}*/
-
-
-
 export class GenericServices {
 
-    public requestGetWithoutBody<T>(url: string): Promise<T>{
+
+
+    public requestPost<T>(url: string, body : object ): Promise<T>{
 
         const requestOptions = {
-            method: 'GET',
+            method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            /*body: JSON.stringify({ title: 'React POST Request Example' })*/
+            body: JSON.stringify(body)
+            //body: body
         };
 
         return this.responseFetcher(url, requestOptions);
     }
 
 
+    public requestGet<T>(url: string, body : object ): Promise<T>{
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+            //body: body
+        };
+
+        return this.responseFetcher(url, requestOptions);
+    }
+
+    public requestGetWithoutBody<T>(url: string ): Promise<T>{
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            
+            //body: body
+        };
+
+        return this.responseFetcher(url, requestOptions);  
+    }
+
     private responseFetcher<T>(url: string, requestOptions: any): Promise<T>{
 
         return fetch(url, requestOptions)
-               .then(response => response.json())  as Promise<T>;
+               .then(response => response.json())
+               .catch(function(error){
+                   console.log('error', error);
+               })  as Promise<T>;
     }
 }
