12ec92598bcf2ad29d55b58c85ab4e1e5b02760d
Eddie Krill <scherenhaenden@gmail.com>
2020-10-13 11:44:50 +0200

    Refactoring and add functions
    -Add type attribute to inputs
    -Complete send registration
    -Add login name property

--- src/pages/registration-component/Register.tsx
+++ src/pages/registration-component/Register.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import InputBoxPlaceholder from '../../components/input-box-placeholder';
+import { RegisterService } from '../../services/register-services';
 import IYoProps from '../login/IYoProps';
 
 interface IState {    
+  loginName: string;
   firstName: string;
   lastName: string;
   createPass: string;
@@ -12,11 +14,13 @@ interface IState {
 
 class Register extends React.Component<IYoProps, IState> {
 
+  private inputTypeLoginName="Login";
   private inputTypeFirstName="First name";
   private inputTypeLastName="Last name";
   private inputTypecreatePass="Create a Password";
   private inputTypeReWritePass="Rewrite your password";  
 
+  private loginName = "";
   private firstName = "";
   private lastName="";
   private createPass="";
@@ -24,6 +28,7 @@ class Register extends React.Component<IYoProps, IState> {
 
 
   state: Readonly<IState> = {
+    loginName: this.loginName,
     firstName: this.firstName,
     lastName: this.lastName,
     createPass: this.createPass,
@@ -31,10 +36,22 @@ class Register extends React.Component<IYoProps, IState> {
   }
 
 
-  private sentRegistration(): void{
+  private sentRegistration(): boolean{
 
-    console.log("this.firstName", this.state.firstName);
+    const registerService = new RegisterService();
 
+    registerService.sendRegistration(this.state.loginName, this.state.firstName, this.state.lastName, this.state.createPass);
+
+    
+
+    return false;
+
+  }
+
+  handlerLogin(value: any ) {
+    this.setState({
+      loginName : value
+    });
   }
 
   handlerFirstName(value: any ) {
@@ -68,20 +85,43 @@ class Register extends React.Component<IYoProps, IState> {
 
 <div>
 
-    <InputBoxPlaceholder  inputName={this.inputTypeFirstName} input={this.state.firstName}  handler={this.handlerFirstName.bind(this)} />
+    <InputBoxPlaceholder  
+    inputName={this.inputTypeLoginName} 
+    input={this.state.loginName}  
+    handler={this.handlerLogin.bind(this)}
+    type="text" />
+    <label /*for="inputEmail"*/>{this.inputTypeLoginName}</label>
+
+    <InputBoxPlaceholder  inputName={this.inputTypeFirstName} input={this.state.firstName}  handler={this.handlerFirstName.bind(this)}
+    type="text"
+    />
     <label /*for="inputEmail"*/>{this.inputTypeFirstName}</label>
 
 
-    <InputBoxPlaceholder  inputName={this.inputTypeLastName} input={this.state.lastName}  handler={this.handlerLastName.bind(this)} />
+    <InputBoxPlaceholder  inputName={this.inputTypeLastName} input={this.state.lastName}  handler={this.handlerLastName.bind(this)}
+    type="text"
+    />
     <label /*for="inputEmail"*/>{this.inputTypeLastName}</label>
 
-    <InputBoxPlaceholder  inputName={this.inputTypecreatePass} input={this.state.createPass}  handler={this.handlerPass1.bind(this)} />
+    <InputBoxPlaceholder  inputName={this.inputTypecreatePass} input={this.state.createPass}  handler={this.handlerPass1.bind(this)} 
+    
+    type="password"
+    />
     <label /*for="inputEmail"*/>{this.inputTypecreatePass}</label>
 
-    <InputBoxPlaceholder  inputName={this.inputTypeReWritePass} input={this.state.reWritePass}  handler={this.handlerPass2.bind(this)} />
+    <InputBoxPlaceholder  inputName={this.inputTypeReWritePass} input={this.state.reWritePass}  handler={this.handlerPass2.bind(this)} 
+    
+    type="password"
+    
+    />
     <label /*for="inputEmail"*/>{this.inputTypeReWritePass}</label>
     
-    <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={(event) => {this.sentRegistration()}}>Sign up</button> 
+    <button className="btn btn-lg btn-primary btn-block" type="button" onClick={(event) => 
+        {
+          event.stopPropagation();
+          this.sentRegistration()
+        }
+      }>Sign up</button> 
     <span>DO NOT USE ANY REAL EMAIL ACCOUNT BECAUSE THIS IS AN EXPERIMENTAL APP</span>       
 </div>  
     );
